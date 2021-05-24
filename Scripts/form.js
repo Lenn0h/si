8491e67a9c4ef9d32b4e38a01f39e6a21621b80c
sebastian <tjonycage@gmail.com>
2021-05-24 16:45:43 -0400

    a

--- Scripts/form.js
+++ Scripts/form.js
@@ -4,8 +4,6 @@ $(document).ready(function()
             $("#formulario").submit(function()
             {
                 var mensaje=""; 
-                var ts=$("#rut-pas");
-                var pas=$("#passrut");
 
                 if($("#nombre").val().trim().length==0)
                 { 
@@ -16,9 +14,17 @@ $(document).ready(function()
                 {
                     mensaje="Ingresa un correo electronico valido, por favor";
                 }
+                if($('#password').val().trim().length<6 ||$('#password').val().trim().length>16){
+                    
+                    mensaje="Tu contraseña debe tener un minimo de 6 carácteres y un maximo de 16";
+                }
                 if($("#ciudad").val().trim().length==0)
                 { 
                     mensaje="El campo ciudad está vacío";
+                }
+                if($("#telefono").val().trim().length<8)
+                {
+                    mensaje="Ingresa un número de telefono valido"
                 }
                 if(mensaje !="")
                 {
