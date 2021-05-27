43a19d64e06351977c2382969fd0d0a120fdaa1c
sebastian <tjonycage@gmail.com>
2021-05-27 16:09:37 -0400

    a

--- Scripts/regWork.js
+++ Scripts/regWork.js
@@ -4,19 +4,20 @@ $(document).ready(function()
             $("#formulario").submit(function()
             {
                 var mensaje=""; 
+                var msjgood="Te contactaremos lo mas pronto posible :), gracias por tu interés!";
 
                 if($("#nombre").val().trim().length==0)
                 { 
                     mensaje="Ingresa un nombre valido, por favor";
                 }
+                if($("#apellido").val().trim().length==0)
+                { 
+                    mensaje="Ingresa un apellido valido, por favor";
+                }
 
                 if($("#email").val().trim().length==0)
                 {
                     mensaje="Ingresa un correo electronico valido, por favor";
-                }
-                if($('#password').val().trim().length<6 ||$('#password').val().trim().length>16){
-                    
-                    mensaje="Tu contraseña debe tener un minimo de 6 carácteres y un maximo de 16";
                 }
                 if($("#ciudad").val().trim().length==0)
                 { 
@@ -25,6 +26,10 @@ $(document).ready(function()
                 if($("#telefono").val().trim().length<8)
                 {
                     mensaje="Ingresa un número de telefono valido"
+                }
+                if($("#comentarios").val().trim().length<10)
+                {
+                    mensaje="Los comentarios deben tener mas de 10 caracteres"
                 }
                 if(mensaje !="")
                 {
